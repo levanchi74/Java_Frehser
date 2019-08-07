fa1ae465f2fa675a0183240e121f850c32b1c8cc
Hai Le <lekhachai2013@gmail.com>
2019-08-07 17:42:32 +0500

    add angularjs project

--- projects/AngularJS/Samples/curd/scripts.js
+++ projects/AngularJS/Samples/curd/scripts.js
@@ -1,16 +1,43 @@
-var app = angular.module("crudApp",[]);
-console.log(app);
+var app = angular.module("crudApp",["ngRoute"]);
+
+app.config(function($routeProvider){
+    $routeProvider
+        .when("/",{
+            templateUrl: "index.htm"
+        })
+        .when("/view",{
+            templateUrl: "viewUser.htm"
+        })
+        .otherwise({
+            redirectTo: "/"
+        });
+})
+
+app.run(['$rootScope', function($rootScope) {
+    $rootScope.users = [
+        {id: 1, firstName:"chi",lastName: "le"},
+        {id: 2, firstName:"tho",lastName: "huynh"},
+        {id: 3, firstName:"tai",lastName: "le"},
+    ];
+
+    
+}]);
 app.controller('crudCtrl',['$scope', function($scope){
 
     if(localStorage.getItem('users') === null){
-        $scope.users = [
-            {id: 1, firstName:"chi",lastName: "le"},
-            {id: 2, firstName:"tho",lastName: "huynh"},
-            {id: 3, firstName:"tai",lastName: "le"},
-        ];
         localStorage.setItem("users",JSON.stringify($scope.users));
     }else{
         $scope.users = JSON.parse(localStorage.getItem('users'));
     }
+
+}]);
+
+app.controller('createUserCtrl',['$scope', function($scope){
+    $scope.id = "";
+    $scope.firstName="";
+    $scope.lastName ="";
+    $scope.addUser = function(){
+        $scope.users.push({id: $scope.id, firstName: $scope.firstName, lastName: $scope.lastName});
+    };
 
 }]);
